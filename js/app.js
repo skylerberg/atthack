de6f7d43e16eedaf4f7bbfd71eace961147a9966
Yang Shu <yangs@punchtab.com>
2014-06-28 11:42:25 -0700

    use angular routes for view navigation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
 (function() {
-    var app = angular.module('myApp', ['onsen.directives']);
+    var app = angular.module('myApp', ['onsen.directives', 'ngRoute']);
+
+    app.config(function($routeProvider){
+        $routeProvider.when('/contact', {
+            templateUrl: 'contactSetup.html',
+            controller: 'ContactCtrl',
+        }).when('/sync', {
+            templateUrl: 'syncTesla.html',
+            controller: 'SyncCtrl'
+        }).when('/main', {
+            templateUrl: 'main.html',
+            controller: 'MainCtrl'
+        }).when('/game', {
+            templateUrl: 'game.html',
+            controller: 'GameCtrl'
+        }).when('/result', {
+            templateUrl: 'result.html',
+            controller: 'ResultCtrl'
+        });
+
+        $routeProvider.otherwise({redirectTo: '/contact'});
+    });
 
     app.factory('Data', function() {
         var Data = {};
@@ -11,7 +32,7 @@
         return Data;
     });
 
-    app.controller('ContactCtrl', function($scope, Data) {
+    app.controller('ContactCtrl', function($scope, Data, $location) {
         $scope.contact = Data.contact;
         $scope.submitted = false;
 
@@ -21,7 +42,20 @@
             for (var key in contact) {
                 window.localStorage.setItem(key, contact[key]);
             }
-            ons.navigator.pushPage("syncTesla.html");
+            $location.path('/sync');
         };
+    });
+
+    app.controller('SyncCtrl', function($scope, Data) {
+
+    });
+    app.controller('MainCtrl', function($scope, Data) {
+
+    });
+    app.controller('GameCtrl', function($scope, Data) {
+
+    });
+    app.controller('ResultCtrl', function($scope, Data) {
+
     });
 })();
