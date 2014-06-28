7b8e9ed5dd94c098a44ad25bd113a4f9dc0c616c
Yang Shu <yangs@punchtab.com>
2014-06-28 13:24:27 -0700

    setups

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 (function() {
+    function LoginFailException(){
+        this.name = "LoginFailException";
+        this.message = "Invalid username or password";
+    }
+    LoginFailException.prototype = Error.prototype;
+
+    function UnAuthorizedException(){
+        this.name = "UnAuthorizedException";
+        this.message = "UnAuthorized";
+    }
+    UnAuthorizedException.prototype = Error.prototype;
+
     var app = angular.module('myApp', ['onsen.directives', 'ngRoute']);
 
     app.config(function($routeProvider){
@@ -19,7 +31,7 @@
             controller: 'ResultCtrl'
         });
 
-        $routeProvider.otherwise({redirectTo: '/contact'});
+        $routeProvider.otherwise({redirectTo: '/sync'});
     });
 
     app.factory('Data', function() {
@@ -32,6 +44,42 @@
         return Data;
     });
 
+    app.service('DummyRESTAPI', function(){
+        isConnected = false;
+
+        this.getLoginStatus = function(){
+            return isConnected;
+        };
+
+        this.login = function(username, password){
+            console.log(password);
+            if (username === 'sober' && password === 'drunk')
+                isConnected = true;
+            else
+                throw new LoginFailException();
+        };
+
+        this.getCarList = function(){
+            if( isConnected === false )
+                throw new UnAuthorizedException();
+            
+            return [
+                {
+                    vehicle_id: '1234567890',
+                    vin: 'KIJPLMNJU789HY7JK'
+                },
+                {
+                    vehicle_id: '0987654321',
+                    vin: 'ARJDLMNJU6Y9GY7JF'
+                }
+            ];
+        };
+    });
+
+    app.controller('FlowController', function($scope){
+
+    });
+
     app.controller('ContactCtrl', function($scope, Data, $location) {
         $scope.contact = Data.contact;
         $scope.submitted = false;
@@ -42,11 +90,29 @@
             for (var key in contact) {
                 window.localStorage.setItem(key, contact[key]);
             }
-            $location.path('/sync');
+            $location.path('/main');
         };
     });
 
-    app.controller('SyncCtrl', function($scope, Data) {
+    app.controller('SyncCtrl', function($scope, Data, DummyRESTAPI, $timeout, $location) {
+        $scope.isConnected = DummyRESTAPI.getLoginStatus();
+        $scope.findMyCar = function(){
+            try{
+                DummyRESTAPI.login($scope.username, $scope.password);
+                $scope.isConnected = DummyRESTAPI.getLoginStatus();
+                $scope.cars = DummyRESTAPI.getCarList();
+                $timeout(function(){
+                    $scope.$apply();
+                });
+            }catch(e){
+                document.getElementById('error-msg').innerHTML = e.message;
+            }
+        };
+
+        $scope.pickMyCar = function(car){
+            window.localStorage.setItem('car', car.vehicle_id);
+            $location.path('/contact');
+        };
 
     });
     app.controller('MainCtrl', function($scope, Data) {
